306f0ad7b22c04bccd53c843c4d77da98a36cb46
Denisi64 <simonpro.denis@gmail.com>
2024-11-26 18:36:28 +0100

    fixture qui passe YEEEEEEEEEEEET

--- src/Enum/CommentStatusEnum.php
+++ src/Enum/CommentStatusEnum.php
@@ -3,7 +3,9 @@
 namespace App\Enum;
 
 enum CommentStatusEnum : string
+
 {
+    public const VALIDATED = 'validated';
     case ACCEPTED = 'accepted';
     case PENDING = 'pending';
     case REJECTED = 'rejected';
