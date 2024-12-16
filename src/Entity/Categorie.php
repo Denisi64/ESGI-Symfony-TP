655cc8159079e78a74fee22e772a891e7d72d366
Denisi64 <simonpro.denis@gmail.com>
2024-12-16 18:09:08 +0100

    Mise en place CRUD

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -6,6 +6,7 @@ use App\Repository\CategorieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
 class Categorie
@@ -15,6 +16,11 @@ class Categorie
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank(message: 'Le nom ne peut pas être vide.')]
+    #[Assert\Length(
+        min: 3,
+        minMessage: 'Le nom doit comporter au moins {{ limit }} caractères.'
+    )]
     #[ORM\Column(length: 100)]
     private ?string $name = null;
 
